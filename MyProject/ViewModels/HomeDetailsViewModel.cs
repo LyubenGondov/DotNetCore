7f948c5d4df84561e7a1e5ac8534ad9b8397b04f
Lyuben <lyuben.gondov@abv.bg>
2019-10-08 19:38:53 +0300

    commit initial

---
+++ MyProject/ViewModels/HomeDetailsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MyProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProject.ViewModels
+{
+    public class HomeDetailsViewModel
+    {
+        public Employee Employee { get; set; }
+        public string PageTitle { get; set; }
+    }
+}
---
+++ MyProject/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyProject.CustomThings;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProject.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Remote(action: "IsEmailInUse", controller: "Account")]
+        [ValidEmailDomain(allowedDomain: "abv.bg", ErrorMessage ="Email domain must be abv.bg!")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password",
+            ErrorMessage = "Password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public string City { get; set; }
+    }
+}
